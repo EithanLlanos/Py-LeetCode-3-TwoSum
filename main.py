8e65fceb9e021a448455516b75f8ff0834165eab
EithanLlanos <eithanllanosb@gmail.com>
2024-04-13 12:39:30 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,62 @@
+# 1. Two Sum
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+ 
+
+# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+
+################################################################################################################################
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #FirstAttempt
+        # for i1,e1 in enumerate(nums):
+        #     for i2,e2 in enumerate(nums):
+        #         if i1==i2: continue
+        #         elif e1+e2==target: return [i1,i2]
+        #Accepted
+        #Runtime ≈ 4469
+        #Memory ≈ 12.50
+
+        #SecondAttempt
+        i1=0
+        while 1:
+            i2=i1+1
+            for e in nums[1:]:
+                if nums[0]+e == target: return [i1,i2]
+                i2+=1
+            i1+=1
+            del nums[0]
+        #Accepted
+        #Runtime ≈ 2183
+        #Memory ≈ 12.45
+            
